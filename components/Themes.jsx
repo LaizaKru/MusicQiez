ca65e5023b402925abe10479e386cc25bf3c37dc
Igor_L <igor101188@gmail.com>
2023-10-27 11:27:13 +0300

    натянул стиль

--- components/Themes.jsx
+++ components/Themes.jsx
@@ -4,38 +4,47 @@ const Layout = require("./Layout");
 function Themes({ themes }) {
   return (
     <Layout>
-      <h1>Выбери тему и кайфуй</h1>
-      <p>
+      <h1 style={{ textAlign: "center", margin: "50px" }}>
+        Выбери тему и кайфуй
+      </h1>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+
+          width: "900px",
+          margin: "0 auto",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
         {themes.map((theme, index) => (
-          <div className="card">
-            <h3>
-              <a href={`/themes/${theme.id}`}></a>
-            </h3>
-            <p>
-              Тема {index}: {theme.title}
-            </p>
-            <div class="card-body">
-              <img
-                class="card-img-top"
-                style={{ width: "200px" }}
-                src={theme.img}
-                alt={theme.title}
-              />
+          <div className="d-flex flex-column align-items-center mb-4">
+            <div className="card " style={{ width: "18rem" }}>
+              <div class="card-body">
+                <img class="card-img-top" src={theme.img} alt={theme.title} />
+              </div>
+              <h5 class="card-title">Тема: {theme.title}</h5>
+
+              <a href={`/themes/${theme.id}`} class="btn btn-primary">
+                Клик тута
+              </a>
             </div>
           </div>
         ))}
-      </p>
+      </div>
     </Layout>
   );
 }
 module.exports = Themes;
 
-// <!-- Карточка с card-img-top -->
-// <div class="card">
-//     <!-- Изображение -->
-//     <img class="card-img-top" src="..." alt="...">
-//     <!-- Текстовый контент -->
-//     <div class="card-body">
-//         <!-- ... -->
-//     </div><
-// </div><!-- Конец карточки -->
+{
+  /* <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div> */
+}
