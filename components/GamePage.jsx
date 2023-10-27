8bd5f9d4a790f75444e1a9812eead8e19ab62581
Igor_L <igor101188@gmail.com>
2023-10-27 15:08:03 +0300

    add music

--- components/GamePage.jsx
+++ components/GamePage.jsx
@@ -8,14 +8,14 @@ function GamePage({title, question}){
     }
     return (
         <Layout><div className='container p-2' id = "game">
-     <h1 class="display-6">{question.question}</h1>
+     <h1 className="display-6">{question.question}</h1>
      {question.question_img && (<img src={question.question_img} />)}
      <form id="answerForm" data-questionId={question.id}>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Ваш ответ</label>
-    <input type="text" name="answer" class="form-control" id="exampleInputEmail1" required/>
+  <div className="mb-3">
+    <label for="exampleInputEmail1" className="form-label">Ваш ответ</label>
+    <input type="text" name="answer" className="form-control" id="exampleInputEmail1" required/>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" className="btn btn-primary">Submit</button>
 </form>
 <div id="result"> </div>
      </div> </Layout>
